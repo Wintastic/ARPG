67703c102e9f126f04e74d47b81f204eddf1ccdd
Julien <julien.mageau@live.ca>
2015-08-15 19:34:04 -0400

    Removed useless comment

--- Assets/Scripts/Player/PlayerNetworkSetup.cs
+++ Assets/Scripts/Player/PlayerNetworkSetup.cs
@@ -5,13 +5,7 @@ using System.Collections;
 public class PlayerNetworkSetup : NetworkBehaviour {
 
 	public override void OnStartLocalPlayer(){
-		//Renderer[] renderers = GetComponentsInChildren<Renderer>();
-		//foreach (Renderer renderer in renderers){
-		//	renderer.enabled = false;
-		//}
-		
-		GetComponent<NetworkAnimator>().SetParameterAutoSend(0, true);
-		
+		GetComponent<NetworkAnimator>().SetParameterAutoSend(0, true);		
 	}
 	
 	public override void PreStartClient(){
